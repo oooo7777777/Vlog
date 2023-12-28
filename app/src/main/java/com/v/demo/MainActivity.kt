67b463318f65f042798225404524df200b38f355
william <452522426@qq.com>
2023-12-28 20:17:45 +0800

    1.2.1,修复zip打包问题

--- app/src/main/java/com/v/demo/MainActivity.kt
+++ app/src/main/java/com/v/demo/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.v.demo
 
-import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.v.log.LogConfig
 import com.v.log.VLog
-import com.v.log.util.ZipUtils.zipFolder
+import com.v.log.util.ZipUtils
 import com.v.log.util.log
 import com.v.log.util.logD
 import com.v.log.util.logE
@@ -16,101 +15,46 @@ import com.v.log.util.logW
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import pub.devrel.easypermissions.EasyPermissions
-import pub.devrel.easypermissions.EasyPermissions.PermissionCallbacks
-import pub.devrel.easypermissions.EasyPermissions.RationaleCallbacks
 
-class MainActivity : AppCompatActivity(), PermissionCallbacks, RationaleCallbacks {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //初始化日志系统
         VLog.init(LogConfig(this, true))
-        EasyPermissions.requestPermissions(
-            this@MainActivity,
-            "申请存储卡选项用于存储",
-            RC_STORAGE_PERM,
-            *WRITE_AND_READ_STORAGE
-        )
         VLog.getDefaultLogPath().log("获取当前日志文件夹目录")//获取当前日志文件夹目录
         VLog.getTodayFilePath().log("获取今日日志")//获取今日日志
 
-
+        writeFile(null)
         //获取所有日志文件
         VLog.getFilesAll().forEach {
             it.log()
         }
 
-        val sb = StringBuffer()
-        repeat(1000)
-        {
-            sb.append("获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录获取当前日志文件夹目录")
-        }
-        sb.logI()
-
-
         //把日志文件打包成zip 并且返回zip地址
-        CoroutineScope(Dispatchers.Main).launch {
-            val zipFilePath = zipFolder(this@MainActivity)
-            ("ZIP 文件路径为：$zipFilePath").log()
+        CoroutineScope(Dispatchers.IO).launch {
+            "ZIP 文件路径为${ZipUtils.zip(this@MainActivity)}".log()
         }
 
     }
 
     fun writeFile(view: View?) {
-        if (hasWriteAndReadPermissions()) {
 
-            try {
+        "logD".logD()
+        Throwable("测试").logE()
+        "错误".logE()
+        "logE".logE("hahhahha")
+        "logW".logW()
+        "logI".logI()
+        "只打印3333333333".log()
+        "只保存444444444444".logSave()
 
-            } catch (e: Exception) {
-                e.toString()
-            }
+        JavaDemo().test()
 
-
-            "logD".logD()
-            Throwable("测试").logE()
-            "错误".logE()
-            "logE".logE("hahhahha")
-            "logW".logW()
-            "logI".logI()
-            "只打印3333333333".log()
-            "只保存444444444444".logSave()
-
-            JavaDemo().test()
-//
-//            Toast.makeText(this@MainActivity, "success", Toast.LENGTH_LONG).show()
-
-        }
-    }
-
-    private fun hasWriteAndReadPermissions(): Boolean {
-        return EasyPermissions.hasPermissions(this, *WRITE_AND_READ_STORAGE)
     }
 
     fun flush(view: View?) {
         VLog.flush()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
-    }
-
-    override fun onPermissionsGranted(requestCode: Int, perms: List<String>) {}
-    override fun onPermissionsDenied(requestCode: Int, perms: List<String>) {}
-    override fun onRationaleAccepted(requestCode: Int) {}
-    override fun onRationaleDenied(requestCode: Int) {}
-
-    companion object {
-        private const val RC_STORAGE_PERM = 123
-        private val WRITE_AND_READ_STORAGE = arrayOf(
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
     }
 
 
--- library/src/main/java/com/v/log/util/ZipUtils.kt
+++ library/src/main/java/com/v/log/util/ZipUtils.kt
@@ -1,144 +1,190 @@
 package com.v.log.util
 
-
 import android.content.Context
 import com.v.log.VLog
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.util.Enumeration
 import java.util.zip.ZipEntry
+import java.util.zip.ZipFile
 import java.util.zip.ZipOutputStream
-
 
 /**
  * author  : ww
  * desc    :
- * time    : 2023/11/29 17:54
+ * time    : 2023/12/28 19:26
  */
 object ZipUtils {
-
-    private fun getZipOutPutPath(context: Context): String {
-        val mPath = File(context.getExternalFilesDir(null), "").absolutePath + "/logs.zip"
-        val logFile = File(mPath)
-        if (!logFile.exists()) {
-            logFile.mkdirs()
-        }
-        return mPath
-    }
-
-    // 打包文件夹为 ZIP，并返回 ZIP 文件的地址
-    suspend fun zipFolder(context: Context): String = coroutineScope {
-        // 定义 ZIP 文件的保存路径
-
-        val zipFilePath = getZipOutPutPath(context)
-
-        // 创建用于写入 ZIP 文件的 ZipOutputStream 对象
-        val zipOutputStream = ZipOutputStream(FileOutputStream(zipFilePath))
-
-        // 获取源文件夹路径对应的 File 对象
-        val sourceFolder = File(VLog.getDefaultLogPath())
-
-        // 获取源文件夹下的所有文件和文件夹
-        val files = sourceFolder.listFiles()
-
-        // 利用协程并行处理文件和文件夹的打包
-        files?.map { file ->
-            async(Dispatchers.IO) {
-                if (file.isDirectory) {
-                    // 如果是文件夹，则递归打包文件夹
-                    zipDirectory(file, "", zipOutputStream)
-                } else {
-                    // 如果是文件，则直接打包文件
-                    zipFile(file, zipOutputStream)
+    fun zip(context: Context): String {
+        //提供了一个数据项压缩成一个ZIP归档输出流
+        var out: ZipOutputStream? = null
+        var outPath = ""
+        try {
+            VLog.flush()
+            outPath = context.getExternalFilesDir(null)!!.absolutePath + "/logs.zip"
+            val outFile = File(outPath) //源文件或者目录
+            val fileOrDirectory = File(VLog.getDefaultLogPath()) //压缩文件路径
+            out = ZipOutputStream(FileOutputStream(outFile))
+            //如果此文件是一个文件，否则为false。
+            if (fileOrDirectory.isFile) {
+                zipFileOrDirectory(out, fileOrDirectory, "")
+            } else {
+                //返回一个文件或空阵列。
+                val entries = fileOrDirectory.listFiles()
+                for (i in entries.indices) {
+                    // 递归压缩，更新curPaths
+                    zipFileOrDirectory(out, entries[i], "")
                 }
             }
-        }?.awaitAll()
-
-        // 关闭 ZipOutputStream
-        zipOutputStream.close()
-
-        // 返回 ZIP 文件的地址
-        zipFilePath
-    }
-
-    // 递归打包文件夹
-    private suspend fun zipDirectory(
-        directory: File,
-        parentPath: String,
-        zipOutputStream: ZipOutputStream
-    ) {
-        // 获取文件夹下的所有文件和文件夹
-        val files = directory.listFiles()
-
-        // 利用协程并行处理文件和文件夹的打包
-        files?.map { file ->
-            CoroutineScope(Dispatchers.Main).async(Dispatchers.IO) {
-                // 构建文件或文件夹的相对路径
-                val relativePath =
-                    if (parentPath.isNotEmpty()) "$parentPath/${file.name}" else file.name
-
-                if (file.isDirectory) {
-                    // 如果是文件夹，则递归打包文件夹
-                    zipDirectory(file, relativePath, zipOutputStream)
-                } else {
-                    // 如果是文件，则直接打包文件
-                    zipFile(file, relativePath, zipOutputStream)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            outPath = ""
+        } finally {
+            //关闭输出流
+            if (out != null) {
+                try {
+                    out.close()
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
                 }
             }
-        }?.awaitAll()
-
-    }
-
-    // 打包单个文件
-    private fun zipFile(file: File, zipOutputStream: ZipOutputStream) {
-        val buffer = ByteArray(1024)
-        val fileInputStream = FileInputStream(file).buffered()
-
-        // 创建 ZipEntry 对象，并将其添加到 ZipOutputStream 中
-        val zipEntry = ZipEntry(file.name)
-        zipOutputStream.putNextEntry(zipEntry)
-
-        var bytesRead: Int
-        while (fileInputStream.read(buffer).also { bytesRead = it } != -1) {
-            // 将文件内容写入 ZipOutputStream
-            zipOutputStream.write(buffer, 0, bytesRead)
         }
-
-        // 关闭当前 ZipEntry
-        zipOutputStream.closeEntry()
-
-        // 关闭文件输入流
-        fileInputStream.close()
+        return outPath
     }
 
-    // 打包单个文件，指定相对路径
-    private fun zipFile(
-        file: File,
-        relativePath: String,
-        zipOutputStream: ZipOutputStream
+    private fun zipFileOrDirectory(
+        out: ZipOutputStream,
+        fileOrDirectory: File, curPath: String
     ) {
-        val buffer = ByteArray(1024)
-        val fileInputStream = FileInputStream(file).buffered()
-
-        // 创建 ZipEntry 对象，并将其添加到 ZipOutputStream 中
-        val zipEntry = ZipEntry(relativePath)
-        zipOutputStream.putNextEntry(zipEntry)
-
-        var bytesRead: Int
-        while (fileInputStream.read(buffer).also { bytesRead = it } != -1) {
-            // 将文件内容写入 ZipOutputStream
-            zipOutputStream.write(buffer, 0, bytesRead)
+        //从文件中读取字节的输入流
+        var `in`: FileInputStream? = null
+        try {
+            //如果此文件是一个目录，否则返回false。
+            if (!fileOrDirectory.isDirectory) {
+                // 压缩文件
+                val buffer = ByteArray(4096)
+                var bytes_read: Int
+                `in` = FileInputStream(fileOrDirectory)
+                //实例代表一个条目内的ZIP归档
+                val entry = ZipEntry(
+                    curPath
+                            + fileOrDirectory.name
+                )
+                //条目的信息写入底层流
+                out.putNextEntry(entry)
+                while (`in`.read(buffer).also { bytes_read = it } != -1) {
+                    out.write(buffer, 0, bytes_read)
+                }
+                out.closeEntry()
+            } else {
+                // 压缩目录
+                val entries = fileOrDirectory.listFiles()
+                for (i in entries.indices) {
+                    // 递归压缩，更新curPaths
+                    zipFileOrDirectory(
+                        out, entries[i], curPath
+                                + fileOrDirectory.name + "/"
+                    )
+                }
+            }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            // throw ex;
+        } finally {
+            if (`in` != null) {
+                try {
+                    `in`.close()
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                }
+            }
         }
+    }
 
-        // 关闭当前 ZipEntry
-        zipOutputStream.closeEntry()
-
-        // 关闭文件输入流
-        fileInputStream.close()
+    fun unzip(zipFileName: String?, outputDirectory: String) {
+        var zipFile: ZipFile? = null
+        try {
+            zipFile = ZipFile(zipFileName)
+            val e: Enumeration<*> = zipFile.entries()
+            var zipEntry: ZipEntry? = null
+            val dest = File(outputDirectory)
+            dest.mkdirs()
+            while (e.hasMoreElements()) {
+                zipEntry = e.nextElement() as ZipEntry
+                val entryName = zipEntry.name
+                var `in`: InputStream? = null
+                var out: FileOutputStream? = null
+                try {
+                    if (zipEntry!!.isDirectory) {
+                        var name = zipEntry.name
+                        name = name.substring(0, name.length - 1)
+                        val f = File(
+                            outputDirectory + File.separator
+                                    + name
+                        )
+                        f.mkdirs()
+                    } else {
+                        var index = entryName.lastIndexOf("\\")
+                        if (index != -1) {
+                            val df = File(
+                                outputDirectory + File.separator
+                                        + entryName.substring(0, index)
+                            )
+                            df.mkdirs()
+                        }
+                        index = entryName.lastIndexOf("/")
+                        if (index != -1) {
+                            val df = File(
+                                outputDirectory + File.separator
+                                        + entryName.substring(0, index)
+                            )
+                            df.mkdirs()
+                        }
+                        val f = File(
+                            outputDirectory + File.separator
+                                    + zipEntry.name
+                        )
+                        // f.createNewFile();
+                        `in` = zipFile.getInputStream(zipEntry)
+                        out = FileOutputStream(f)
+                        var c: Int
+                        val by = ByteArray(1024)
+                        while (`in`.read(by).also { c = it } != -1) {
+                            out.write(by, 0, c)
+                        }
+                        out.flush()
+                    }
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                    throw IOException("解压失败：$ex")
+                } finally {
+                    if (`in` != null) {
+                        try {
+                            `in`.close()
+                        } catch (ex: IOException) {
+                        }
+                    }
+                    if (out != null) {
+                        try {
+                            out.close()
+                        } catch (ex: IOException) {
+                        }
+                    }
+                }
+            }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            throw IOException("解压失败：$ex")
+        } finally {
+            if (zipFile != null) {
+                try {
+                    zipFile.close()
+                } catch (ex: IOException) {
+                }
+            }
+        }
     }
 }
